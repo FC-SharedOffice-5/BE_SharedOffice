4cebcce8fd8c12106ab8fbf3ad7c59ea8cf1a762
JeongByungJoo <cheng286@naver.com>
2024-05-14 23:41:45 +0900

    feat : 프로젝트 생성

---
+++ src/test/java/com/FC/SharedOfficePlatform/SharedOfficePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FC.SharedOfficePlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedOfficePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
