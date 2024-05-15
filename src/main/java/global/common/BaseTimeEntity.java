9f2093a17da2391b4c5745f80407ef7c5a26c283
JeongByungJoo <cheng286@naver.com>
2024-05-15 18:10:15 +0900

    refactor: ErrorCode의 User -> Member로 이름 변경 및 불필요한 import문 제거

--- src/main/java/global/common/BaseTimeEntity.java
+++ src/main/java/global/common/BaseTimeEntity.java
@@ -1,6 +1,5 @@
 package global.common;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
--- src/main/java/global/exception/ErrorCode.java
+++ src/main/java/global/exception/ErrorCode.java
@@ -3,19 +3,15 @@ package global.exception;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
-
 @Getter
 public enum ErrorCode {
 
-
     // USER
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 회원입니다."),
-    USER_ALREADY_REGISTERED(HttpStatus.BAD_REQUEST, "이미 가입된 회원입니다."),
-
+    MEMBER_ALREADY_REGISTERED(HttpStatus.BAD_REQUEST, "이미 가입된 회원입니다."),
 
     // 5xx
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 에러");
-
 
     private HttpStatus httpStatus;
     private String message;
