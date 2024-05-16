17c91c49a94eb6a93b0cf9242bb9ce57bb64feaf
JeongByungJoo <cheng286@naver.com>
2024-05-16 14:25:27 +0900

    feat: Member 도메인 관련 패키지 및 클래스 생성 및 기본 설정 추가, dto의 request와 response는 이름 변경 용이하도록 간단하게 작성 (패키지 구조의 커밋이 남으려면 class 파일이 필요하여 함께 작성해둠)

---
+++ src/main/java/com/FC/SharedOfficePlatform/domain/member/dto/request/request.java
@@ -0,0 +1,5 @@
+package com.FC.SharedOfficePlatform.domain.member.dto.request;
+
+public record request() {
+
+}
---
+++ src/main/java/com/FC/SharedOfficePlatform/domain/member/dto/response/response.java
@@ -0,0 +1,5 @@
+package com.FC.SharedOfficePlatform.domain.member.dto.response;
+
+public record response() {
+
+}
