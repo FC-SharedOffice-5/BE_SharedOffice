5b6fa9bf205625fc4347d52ee2858f76c1115de1
JeongByungJoo <cheng286@naver.com>
2024-05-16 14:18:07 +0900

    feat: Reservation 도메인 관련 패키지 생성, dto의 request와 response는 이름 변경 용이하도록 간단하게 작성 (패키지 구조의 커밋이 남으려면 class 파일이 필요하여 함께 작성해둠)

---
+++ src/main/java/com/FC/SharedOfficePlatform/domain/reservation/dto/request/request.java
@@ -0,0 +1,5 @@
+package com.FC.SharedOfficePlatform.domain.reservation.dto.request;
+
+public record request() {
+
+}
---
+++ src/main/java/com/FC/SharedOfficePlatform/domain/reservation/dto/response/response.java
@@ -0,0 +1,5 @@
+package com.FC.SharedOfficePlatform.domain.reservation.dto.response;
+
+public record response() {
+
+}
