544277f486e79f4355329a6eea199ef3a2d3b057
JeongByungJoo <cheng286@naver.com>
2024-05-27 00:48:03 +0900

    feat: JavaMailConfig 클래스 추가
    상세설명
    1. 이메일 인증 보내기 API를 위한 JavaMailSender의 설정 추가

---
+++ src/main/java/com/FC/SharedOfficePlatform/global/config/JavaMailConfig.java
@@ -0,0 +1,38 @@
+package com.FC.SharedOfficePlatform.global.config;
+
+import java.util.Properties;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class JavaMailConfig {
+
+    @Value("${EMAIL_ID}")
+    private String emailId;
+
+    @Value("${EMAIL_PASSWORD}")
+    private String emailPassword;
+
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.naver.com");
+        mailSender.setPort(465);
+        mailSender.setUsername(emailId);
+        mailSender.setPassword(emailPassword);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.ssl.enable", "true"); // Enable SSL
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
+
