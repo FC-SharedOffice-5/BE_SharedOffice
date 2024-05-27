9770a9c213a311b7fbd7e228d2a3adbda2528b92
yebin <limyebin1218@naver.com>
2024-05-28 03:45:00 +0900

    feat: 오피스 지점 등록 / 조회 / 상세조회 request 1차 개발

---
+++ src/main/java/com/FC/SharedOfficePlatform/domain/office/dto/request/OfficeRequest.java
@@ -0,0 +1,31 @@
+package com.FC.SharedOfficePlatform.domain.office.dto.request;
+
+import com.FC.SharedOfficePlatform.domain.office.entity.Office;
+
+public record OfficeRequest(
+        Long officeId,
+        String officeName,
+        String officeAddr,
+        int officeFloor,
+        String officeTime,
+        int officeCapacity,
+        int officeStudio,
+        int officeMeeting,
+        String officeLatitude,
+        String officeLongitude
+) {
+    public Office toEntity() {
+        return Office
+                .builder()
+                .officeName(officeName())
+                .officeAddr(officeAddr())
+                .officeFloor(officeFloor())
+                .officeTime(officeTime())
+                .officeCapacity(officeCapacity())
+                .officeStudio(officeStudio())
+                .officeMeeting(officeMeeting())
+                .officeLatitude(officeLatitude())
+                .officeLongitude(officeLongitude())
+                .build();
+    }
+}
