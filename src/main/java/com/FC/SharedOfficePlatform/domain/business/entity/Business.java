1035befa557f61f0f1a279a86862ffdc523b49d2
yebin <limyebin1218@naver.com>
2024-05-28 01:23:05 +0900

    feat: 입점 기업 등록 / 조회 / 상세조회 Entity 1차 개발

--- src/main/java/com/FC/SharedOfficePlatform/domain/business/entity/Business.java
+++ src/main/java/com/FC/SharedOfficePlatform/domain/business/entity/Business.java
@@ -1,20 +1,73 @@
 package com.FC.SharedOfficePlatform.domain.business.entity;
 
 import com.FC.SharedOfficePlatform.global.common.BaseTimeEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @Entity
+@Table(name = "business")
 @NoArgsConstructor
 @Getter
 public class Business extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Column(name = "business_id", nullable = false)
+    private Long businessId;
+
+    @Column(name = "business_name", length = 50, nullable = false)
+    private String businessName;
+
+    @Column(name = "business_type", length = 6)
+    private String businessType;
+
+    @Column(name = "business_num")
+    private int businessNum;
+
+    @Column(name = "business_contract_start_date")
+    private LocalDate businessContractStartDate;
+
+    @Column(name = "business_contract_end_date")
+    private LocalDate businessContractEndDate;
+
+    @Column(name = "business_ceo", length = 25)
+    private String businessCeo;
+
+    @Column(name = "business_phone", length = 13)
+    private String businessPhone;
+
+    @Column(name = "business_contract_person", length = 25)
+    private String businessContractPerson;
+
+    @Column(name = "business_contract_phone", length = 13)
+    private String businessContractPhone;
+
+
+    @Builder
+    public Business(
+            String businessName,
+            String businessType,
+            int businessNum,
+            LocalDate businessContractStartDate,
+            LocalDate businessContractEndDate,
+            String businessCeo,
+            String businessPhone,
+            String businessContractPerson,
+            String businessContractPhone
+    ) {
+        this.businessName = businessName;
+        this.businessType = businessType;
+        this.businessNum = businessNum;
+        this.businessContractStartDate = businessContractStartDate;
+        this.businessContractEndDate = businessContractEndDate;
+        this.businessCeo = businessCeo;
+        this.businessPhone = businessPhone;
+        this.businessContractPerson = businessContractPerson;
+        this.businessContractPhone = businessContractPhone;
+    }
 
 }
