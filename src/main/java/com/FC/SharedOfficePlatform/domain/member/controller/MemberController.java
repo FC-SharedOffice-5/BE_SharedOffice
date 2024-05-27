42acafc9bdbdb924e8928b3b193994c430dbcf42
JeongByungJoo <cheng286@naver.com>
2024-05-27 22:48:33 +0900

    feat: 비밀번호 수정 - MemberController 해당 API 추가
    상세설명
    1. MemberController의 URl 수정
    MemberController의 RequestMapping 부분 ("/members") 추가
    2. 비밀번호 수정 : updatePassword PUT method 추가

--- src/main/java/com/FC/SharedOfficePlatform/domain/member/controller/MemberController.java
+++ src/main/java/com/FC/SharedOfficePlatform/domain/member/controller/MemberController.java
@@ -1,27 +1,31 @@
 package com.FC.SharedOfficePlatform.domain.member.controller;
 
 import com.FC.SharedOfficePlatform.domain.member.dto.request.SignUpMemberRequest;
+import com.FC.SharedOfficePlatform.domain.member.dto.request.UpdatePasswordRequest;
 import com.FC.SharedOfficePlatform.domain.member.dto.response.SignUpMemberResponse;
+import com.FC.SharedOfficePlatform.domain.member.dto.response.UpdatePasswordResponse;
 import com.FC.SharedOfficePlatform.domain.member.service.MemberService;
 import com.FC.SharedOfficePlatform.global.util.ResponseDTO;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/signup")
+@RequestMapping("/members")
 @Slf4j
 public class MemberController {
 
     private final MemberService memberService;
 
-    @PostMapping
+    @PostMapping("/signup")
     public ResponseEntity<ResponseDTO<SignUpMemberResponse>> signUp(
         @RequestBody @Valid SignUpMemberRequest request
     ) {
@@ -29,6 +33,14 @@ public class MemberController {
 
         SignUpMemberResponse response = memberService.registerNewMember(request);
 
+        return ResponseEntity.ok(ResponseDTO.okWithData(response));
+    }
+
+    @PutMapping("/update/pw/{id}")
+    public ResponseEntity<ResponseDTO<UpdatePasswordResponse>> updatePassword(
+        @PathVariable Long id, @RequestBody UpdatePasswordRequest request
+    ) {
+        UpdatePasswordResponse response = memberService.updatePassword(id, request);
         return ResponseEntity.ok(ResponseDTO.okWithData(response));
     }
 }
