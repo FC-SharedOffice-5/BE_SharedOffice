8c916601b0ebbcfdb58fd3e67a2d638ea9752be5
yebin <limyebin1218@naver.com>
2024-05-28 17:39:48 +0900

    feat: 오피스/스튜디오 장소 등록 / 조회 / 상세조회 controller 1차 개발

--- src/main/java/com/FC/SharedOfficePlatform/domain/place/controller/PlaceController.java
+++ src/main/java/com/FC/SharedOfficePlatform/domain/place/controller/PlaceController.java
@@ -1,15 +1,39 @@
 package com.FC.SharedOfficePlatform.domain.place.controller;
 
+import com.FC.SharedOfficePlatform.domain.place.dto.request.PlaceRequest;
+import com.FC.SharedOfficePlatform.domain.place.dto.response.PlaceListResponse;
+import com.FC.SharedOfficePlatform.domain.place.dto.response.PlaceResponse;
 import com.FC.SharedOfficePlatform.domain.place.service.PlaceService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/")
+@RequestMapping("/places")
 public class PlaceController {
 
     private final PlaceService placeService;
+
+    @PostMapping
+    public ResponseEntity<PlaceResponse> insertPlace(@RequestBody PlaceRequest request) {
+        PlaceResponse placeResponse = placeService.insertPlace(request);
+        return new ResponseEntity<>(placeResponse, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PlaceListResponse>> getAllPlace() {
+        List<PlaceListResponse> placeListResponses = placeService.getAllPlace();
+        return ResponseEntity.ok(placeListResponses);
+    }
+
+    @GetMapping("/{placeId}")
+    public ResponseEntity<PlaceResponse> getDetailPlace(@PathVariable Long PlaceId) {
+        PlaceResponse placeResponse = placeService.getDetailPlace(PlaceId);
+        return ResponseEntity.ok(placeResponse);
+    }
 
 }
