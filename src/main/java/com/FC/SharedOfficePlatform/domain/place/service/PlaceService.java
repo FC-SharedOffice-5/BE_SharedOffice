18587e0f5a9604f824ae4f4bcaa949c1785ed082
yebin <limyebin1218@naver.com>
2024-05-28 17:40:07 +0900

    feat: 오피스/스튜디오 장소 등록 / 조회 / 상세조회 service 1차 개발

--- src/main/java/com/FC/SharedOfficePlatform/domain/place/service/PlaceService.java
+++ src/main/java/com/FC/SharedOfficePlatform/domain/place/service/PlaceService.java
@@ -1,15 +1,54 @@
 package com.FC.SharedOfficePlatform.domain.place.service;
 
+import com.FC.SharedOfficePlatform.domain.office.dto.OfficeDto;
+import com.FC.SharedOfficePlatform.domain.office.dto.request.OfficeRequest;
+import com.FC.SharedOfficePlatform.domain.office.dto.response.OfficeListResponse;
+import com.FC.SharedOfficePlatform.domain.office.dto.response.OfficeResponse;
+import com.FC.SharedOfficePlatform.domain.office.entity.Office;
+import com.FC.SharedOfficePlatform.domain.office.exception.OfficeNotFoundException;
+import com.FC.SharedOfficePlatform.domain.place.dto.request.PlaceRequest;
+import com.FC.SharedOfficePlatform.domain.place.dto.response.PlaceListResponse;
+import com.FC.SharedOfficePlatform.domain.place.dto.response.PlaceResponse;
+import com.FC.SharedOfficePlatform.domain.place.entity.Place;
+import com.FC.SharedOfficePlatform.domain.place.exception.PlaceNotFoundException;
 import com.FC.SharedOfficePlatform.domain.place.repository.PlaceRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
+@Slf4j
 public class PlaceService {
 
     private final PlaceRepository placeRepository;
+
+    @Transactional
+    public PlaceResponse insertPlace(PlaceRequest PlaceRequest) {
+        Place place = PlaceRequest.toEntity();
+        Place savePlace = placeRepository.save(place);
+        return PlaceResponse.from(savePlace);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PlaceListResponse> getAllPlace() {
+        return placeRepository.findAll().stream()
+                .map(PlaceListResponse::from)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public PlaceResponse getDetailPlace(Long PlaceId) {
+        Place Place = placeRepository.findById(PlaceId)
+                .orElseThrow(() -> {
+                    log.error("Place with ID {} not found", PlaceId); // 로그 추가
+                    return new PlaceNotFoundException("Inquiry with ID " + PlaceId + " not found");
+                });
+        return PlaceResponse.from(Place);
+    }
 
 }
