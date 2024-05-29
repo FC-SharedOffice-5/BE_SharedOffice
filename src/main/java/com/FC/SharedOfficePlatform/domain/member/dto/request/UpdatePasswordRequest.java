89ac6c62f4586f60e10f901b37083bb589cce7c2
JeongByungJoo <cheng286@naver.com>
2024-05-29 13:32:33 +0900

    fix : UpdatePasswordRequest 수정
    상세설명
    1. validation 형식에서 비밀번호 필드에 이메일 관련 내용이 있어 수정

--- src/main/java/com/FC/SharedOfficePlatform/domain/member/dto/request/UpdatePasswordRequest.java
+++ src/main/java/com/FC/SharedOfficePlatform/domain/member/dto/request/UpdatePasswordRequest.java
@@ -2,10 +2,11 @@ package com.FC.SharedOfficePlatform.domain.member.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record UpdatePasswordRequest(
-    @NotNull(message = "이메일을 입력해 주세요.")
-    @Email(message = "이메일 형식이 유효하지 않습니다.")
+    @NotNull(message = "비밀 번호를 입력해 주세요.")
+    @Size(min = 8, message = "비밀 번호는 8자 이상 20자 이하입니다.")
     String password
 ) {
 
