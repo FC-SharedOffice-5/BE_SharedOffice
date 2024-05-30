8eea477135f4dc61f5fc8f3599c6a2389108df92
JeongByungJoo <cheng286@naver.com>
2024-05-30 13:15:27 +0900

    feat:내 정보 수정 MyPageController
    상세설명
    1. updateMyInfo() : PathVariable 을 활용한 PATCH method 추가

--- src/main/java/com/FC/SharedOfficePlatform/domain/member/controller/MyPageController.java
+++ src/main/java/com/FC/SharedOfficePlatform/domain/member/controller/MyPageController.java
@@ -1,12 +1,17 @@
 package com.FC.SharedOfficePlatform.domain.member.controller;
 
+import com.FC.SharedOfficePlatform.domain.member.dto.request.UpdateMyInfoRequest;
 import com.FC.SharedOfficePlatform.domain.member.dto.response.MyInfoResponse;
+import com.FC.SharedOfficePlatform.domain.member.dto.response.UpdateMyInfoResponse;
 import com.FC.SharedOfficePlatform.domain.member.service.MyPageService;
 import com.FC.SharedOfficePlatform.global.util.ResponseDTO;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +27,14 @@ public class MyPageController {
         @PathVariable Long memberId
     ) {
         MyInfoResponse response = myPageService.getMyInfo(memberId);
+        return ResponseEntity.ok(ResponseDTO.okWithData(response));
+    }
+
+    @PatchMapping("/update/info/{memberId}")
+    public ResponseEntity<ResponseDTO<UpdateMyInfoResponse>> updateMyInfo(
+        @PathVariable Long memberId, @RequestBody @Valid UpdateMyInfoRequest request
+    ) {
+        UpdateMyInfoResponse response = myPageService.updateMyInfo(memberId, request);
         return ResponseEntity.ok(ResponseDTO.okWithData(response));
     }
 }
