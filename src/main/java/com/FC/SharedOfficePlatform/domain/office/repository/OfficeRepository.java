661c8afdada610272ee90f9a08932a05e884a054
yebin <limyebin1218@naver.com>
2024-06-01 18:20:25 +0900

    refactor: 지점 검색 기능 추가

--- src/main/java/com/FC/SharedOfficePlatform/domain/office/repository/OfficeRepository.java
+++ src/main/java/com/FC/SharedOfficePlatform/domain/office/repository/OfficeRepository.java
@@ -4,7 +4,9 @@ import com.FC.SharedOfficePlatform.domain.office.entity.Office;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OfficeRepository extends JpaRepository<Office, Long> {
-
+    List<Office> findByOfficeNameContaining(String officeName);
 }
