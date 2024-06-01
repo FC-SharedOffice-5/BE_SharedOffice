bdbef4d943ecdefee4d789b9bd5fc5cd19f282f7
yebin <limyebin1218@naver.com>
2024-06-02 07:25:00 +0900

    feat: 장소 예약 층별 조회 / 등록 / 조회 controller 1차 개발

---
+++ src/main/java/com/FC/SharedOfficePlatform/domain/placeReservation/controller/PlaceReservationController.java
@@ -0,0 +1,41 @@
+package com.FC.SharedOfficePlatform.domain.placeReservation.controller;
+
+import com.FC.SharedOfficePlatform.domain.place.dto.response.PlaceDetailResponse;
+import com.FC.SharedOfficePlatform.domain.placeReservation.dto.request.PlaceReservationRequest;
+import com.FC.SharedOfficePlatform.domain.placeReservation.dto.response.PlaceReservationListResponse;
+import com.FC.SharedOfficePlatform.domain.placeReservation.dto.response.PlaceReservationResponse;
+import com.FC.SharedOfficePlatform.domain.placeReservation.service.PlaceReservationService;
+import com.FC.SharedOfficePlatform.global.util.ResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+public class PlaceReservationController {
+
+    private final PlaceReservationService placeReservationService;
+
+    @GetMapping("/offices/places/reservations")
+    public ResponseEntity<ResponseDTO<List<PlaceDetailResponse>>> getDetailPlaceFloor(@RequestParam int placeFloor) {
+        List<PlaceDetailResponse> placeDetailResponse = placeReservationService.getDetailPlaceFloor(placeFloor);
+        return ResponseEntity.ok(ResponseDTO.okWithData(placeDetailResponse));
+    }
+
+    @PostMapping("/offices/places/reservations")
+    public ResponseEntity<PlaceReservationResponse> insertPlaceReservation(@RequestBody PlaceReservationRequest request) {
+        PlaceReservationResponse placeResponse = placeReservationService.insertPlaceReservation(request);
+        return new ResponseEntity<>(placeResponse, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/mypage/reservations")
+    public ResponseEntity<ResponseDTO<Map<Long, List<PlaceReservationListResponse>>>> getAllPlaceReservation(Long memberId) {
+        Map<Long, List<PlaceReservationListResponse>> placeReservationDetail = placeReservationService.getAllPlaceReservation(memberId);
+        return ResponseEntity.ok(ResponseDTO.okWithData(placeReservationDetail));
+    }
+
+}
