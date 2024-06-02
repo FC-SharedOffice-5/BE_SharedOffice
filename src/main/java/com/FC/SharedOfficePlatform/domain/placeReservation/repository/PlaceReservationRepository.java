834275ea383018a0f9cd9cb7fec2fb34c0247c61
yebin <limyebin1218@naver.com>
2024-06-03 00:10:19 +0900

    refactor: 지점 층별 쿼리문 작성

--- src/main/java/com/FC/SharedOfficePlatform/domain/placeReservation/repository/PlaceReservationRepository.java
+++ src/main/java/com/FC/SharedOfficePlatform/domain/placeReservation/repository/PlaceReservationRepository.java
@@ -2,7 +2,9 @@ package com.FC.SharedOfficePlatform.domain.placeReservation.repository;
 
 import com.FC.SharedOfficePlatform.domain.place.entity.Place;
 import com.FC.SharedOfficePlatform.domain.placeReservation.entity.PlaceReservation;
+import com.FC.SharedOfficePlatform.domain.seatReservation.entity.SeatReservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -12,5 +14,8 @@ import java.util.List;
 public interface PlaceReservationRepository extends JpaRepository<PlaceReservation, Long> {
     List<PlaceReservation> findByPlace_Office_OfficeId(Long officeId);
 
-    List<PlaceReservation> findByMemberId(Long memberId); // Member 엔티티의 memberId를 기준으로 조회
+    @Query("SELECT pr FROM PlaceReservation pr WHERE pr.place.officeId = :officeId AND pr.place.placeFloor = :placeFloor")
+    List<PlaceReservation> findByPlaceFloorAndOfficeId(@Param("officeId") Long officeId, @Param("placeFloor") int placeFloor);
+
+    List<PlaceReservation> findByMemberId(Long memberId);
 }
