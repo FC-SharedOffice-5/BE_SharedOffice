ffc483381d98deac5d6edc9cf97d13f01f3da138
JeongByungJoo <cheng286@naver.com>
2024-06-10 23:37:47 +0900

    feat: 오피스 이미지 : Response
    상세설명
    1. 이미지에 officeId 가 있는 경우 반환할 수 있도록 해당 내용 추가

--- src/main/java/com/FC/SharedOfficePlatform/domain/image/dto/ImageDataResponse.java
+++ src/main/java/com/FC/SharedOfficePlatform/domain/image/dto/ImageDataResponse.java
@@ -6,17 +6,20 @@ public record ImageDataResponse(
     String fileName,
     String url,
     Long memberId,
-    Long boardId
+    Long boardId,
+    Long officeId
 ) {
     public static ImageDataResponse from(ImageData imageData) {
         Long memberId = (imageData.getMember() != null) ? imageData.getMember().getId() : null;
         Long boardId = (imageData.getFreeBoard() != null) ? imageData.getFreeBoard().getBoardId() : null;
+        Long officeId = (imageData.getOffice() != null) ? imageData.getOffice().getOfficeId() : null;
         return new ImageDataResponse(
             imageData.getId(),
             imageData.getName(),
             imageData.getUrl(),
             memberId,
-            boardId
+            boardId,
+            officeId
         );
     }
 }
