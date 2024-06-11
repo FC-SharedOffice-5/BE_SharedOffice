1513d4b94cc6cbad1da5accdc8ef65d8d0006e2e
JeongByungJoo <cheng286@naver.com>
2024-06-11 18:45:14 +0900

    feat: 장소 이미지 조회 : Response
    상세설명
    1. ImageDataResponse에 placeId가 존재하는 경우 반환할 수 있도록 추가

--- src/main/java/com/FC/SharedOfficePlatform/domain/image/dto/ImageDataResponse.java
+++ src/main/java/com/FC/SharedOfficePlatform/domain/image/dto/ImageDataResponse.java
@@ -7,19 +7,22 @@ public record ImageDataResponse(
     String url,
     Long memberId,
     Long boardId,
-    Long officeId
+    Long officeId,
+    Long placeId
 ) {
     public static ImageDataResponse from(ImageData imageData) {
         Long memberId = (imageData.getMember() != null) ? imageData.getMember().getId() : null;
         Long boardId = (imageData.getFreeBoard() != null) ? imageData.getFreeBoard().getBoardId() : null;
         Long officeId = (imageData.getOffice() != null) ? imageData.getOffice().getOfficeId() : null;
+        Long placeId = (imageData.getPlace() != null) ? imageData.getPlace().getPlaceId() : null;
         return new ImageDataResponse(
             imageData.getId(),
             imageData.getName(),
             imageData.getUrl(),
             memberId,
             boardId,
-            officeId
+            officeId,
+            placeId
         );
     }
 }
